e7d88f905e7c011a8f5f6d8620805a8ad759792f
joeldevel <christianjoel.developer@gmail.com>
2022-01-28 18:20:07 -0300

    adds form and input to App. Adds state to App

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import React from 'react';
 import Overview from './components/Overview';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
+        this.state = {
+            task: {text: ''},
+            tasks: [{text: 'finish The odin project'}],
+        };
     }
 
     render() {
         return(
             <div className="app">
-                <h1> it works</h1>
-                <Overview />
+                <h1>Taskinator</h1>
+                <form>
+                    <input type="text" name="task"/>
+                    <button type="submit">Add task</button>
+                </form>
+                {this.state.tasks.length === 0 ?
+                    <h2> There are no tasks</h2> :
+                    this.state.tasks.map(task => <Overview task={task}/>)
+                }
             </div>
         );
     }
