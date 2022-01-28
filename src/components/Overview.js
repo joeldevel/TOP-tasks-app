a4ccff573b94ee97149560c93d40ce92a5da3e00
joeldevel <christianjoel.developer@gmail.com>
2022-01-28 20:20:35 -0300

    tasks list displays its number

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -4,10 +4,14 @@
 function Overview({tasks, deleteTask}) {
     return (
         <ul>
-            {tasks.map(task => {
+            {tasks.map((task, i) => {
                 return (
-                    <li key={task.id}>{task.text}
-                        <button type="button" onClick={()=>deleteTask(task.id)}>X</button>
+                    <li key={task.id}>
+                        <span className="task-number">{i+1} </span>
+                        <p>{task.text}</p>
+                        <button type="button"
+                                className="delete-task"
+                                onClick={()=>deleteTask(task.id)}>X</button>
                     </li>
                 )
             })}
